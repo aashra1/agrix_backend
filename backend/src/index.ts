9280ceb8fa8ef76070c3fab4829724cb293982a6
aashra1 <aashrapandey00@gmail.com>
2026-01-30 11:20:47 +0545

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import businessRoutes from "./routes/business.routes";
 import productRoutes from "./routes/product.routes";
 import categoryRoutes from "./routes/category.routes";
 import connectDB from "./database/db";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ connectDB();
 
 // Middleware
 app.use(bodyParser.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Register Routes
 app.use("/api/user", userRoutes);
